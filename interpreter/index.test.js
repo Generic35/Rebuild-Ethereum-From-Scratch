200d1ec09fb457767fa816dc05e955fb6256523c
Generic35 <ricklamontagne@gmail.com>
2025-09-17 14:31:52 -0400

    feat(interpreter): Create the interpreter for the on-chain language (similar to solidity op-code) that will be used for this Ethereum blockchain clone

---
+++ interpreter/index.test.js
@@ -0,0 +1,20 @@
+const Interpreter = require('./index');
+const {
+  STOP,
+  ADD,
+  SUB,
+  MUL,
+  DIV,
+  PUSH,
+  LT,
+  GT,
+  EQ,
+  AND,
+  OR,
+  JUMP,
+  JUMPI
+} = Interpreter.OPCODE_MAP;
+
+describe('Interpreter', () => {
+  // add interpreter test cases here
+});
