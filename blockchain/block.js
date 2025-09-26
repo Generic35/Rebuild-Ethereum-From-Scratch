d802d2a8cd7e482bfc1caee27fb8701b6321779e
Generic35 <ricklamontagne@gmail.com>
2025-09-26 09:40:08 -0400

    feat: add the ability to calculate the block's target hash and use it to mine block, together they implement the proof of work

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,13 +1,55 @@
 const { GENESIS_DATA } = require('../config');
+const { keccakHash } = require('../util');
 
+const HASH_LENGTH = 64;
+const MAX_HASH_VALUE = parseInt('f'.repeat(HASH_LENGTH), 16);
+const MAX_NONCE_VALUE = 2 ** 64;
 class Block {
-  constructor({ blockHeaders }) {
-    this.blockHeaders = blockHeaders;
-  }
+	constructor({ blockHeaders }){
+		this.blockHeaders = blockHeaders;
+	}
 
-  static genesis() {
-    return new Block(GENESIS_DATA);
-  }
+	static calculateBlockTargetHash({lastBlock}) {
+		const value = (MAX_HASH_VALUE / lastBlock.blockHeaders.difficulty).toString(16);
+
+		if(value.length > HASH_LENGTH) {
+			return 'f'.repeat(HASH_LENGTH);
+		}
+
+		return '0'.repeat(HASH_LENGTH - value.length) + value;
+	}
+	
+	static mineBlock({ lastBlock, beneficiary }) {
+		const target = Block.calculateBlockTargetHash({ lastBlock })
+		let timestamp, truncatedBlockHeaders, header, nonce, underTargetHash
+
+		do {
+			timestamp = Date.now();
+			truncatedBlockHeaders = {
+				parentHash: keccakHash(lastBlock.blockHeaders),
+				beneficiary,
+				difficulty: lastBlock.blockHeaders.difficulty + 1,
+				number: lastBlock.blockHeaders.number + 1,
+				timestamp
+			}
+			header = keccakHash(truncatedBlockHeaders);
+			nonce = Math.floor(Math.random() * MAX_NONCE_VALUE);
+	
+			underTargetHash = keccakHash(header + nonce);
+		} while (underTargetHash > target);
+
+	
+
+		return new this({
+			blockHeaders: {
+				...truncatedBlockHeaders, nonce }
+			
+		})
+	}
+
+	static genesis() {
+		return new this(GENESIS_DATA);
+	}
 }
 
 module.exports = Block;
